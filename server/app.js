b91b538322f92b1fb33fbfe2dcda9dccb36503f0
Omkaarr <pallapoluomkar@gmail.com>
2021-06-18 21:05:39 +0530

    Main

---
+++ server/app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const cors = require('cors')
+const { graphqlHTTP } = require('express-graphql')
+const mongoose = require('mongoose')
+const schema = require('./schema/schema')
+require('dotenv').config()
+
+const app = express()
+
+
+app.use(cors())
+
+mongoose.connect(process.env.mongodbUri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
+
+mongoose.connection.once('open', () => console.log('Connected to database'))
+
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}))
+
+if(process.env.NODE_ENV == "production")
+{
+    app.use(express.static("client/build"));
+}
+
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log(`Server is up at port ${port}`))
---
+++ server/models/Word.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const wordShema = new mongoose.Schema({
+    word: { type: String, unique: true },
+    entries: [{
+        partOfSpeech: { type: String },
+        origin: [{ type: String }],
+        definitions: [{ type: String }],
+        examples: [{ type: String }]
+    }]
+})
+
+
+module.exports = mongoose.model('Word', wordShema)
