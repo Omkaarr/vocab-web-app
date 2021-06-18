d0271bd52b81b7fa209bfa8ba22b4832192d430e
Omkaarr <pallapoluomkar@gmail.com>
2021-06-18 22:06:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,12 +20,10 @@ app.use('/graphql', graphqlHTTP({
     graphiql: true
 }))
 
-if (process.env.NODE_ENV === "production"{
-    app.use(express.static("build"));
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname,  "build", "index.html"));
-    });
-  }
+if(process.env.NODE_ENV == "production")
+{
+    app.use(express.static("client/build"));
+}
 
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Server is up at port ${port}`))
